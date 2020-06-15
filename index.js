9fd2843859986f963af12a0fbbb43083f9df9422
wangchufeng <941994464@qq.com>
2020-06-15 16:31:15 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('http').createServer(handler)
+const io = require('socket.io')(app);
+const fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/client.html',
+  (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
+});
